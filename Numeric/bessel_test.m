ae113f78ca7647a95852c1c5ae7f174536bc4225
M.P. Vaughan <martinpaul.vaughan@yahoo.com>
2020-04-22 18:08:21 +0100

    The routine bessel_test takes an argument nu and plots J(nu,z).

--- Numeric/bessel_test.m
+++ Numeric/bessel_test.m
@@ -1,7 +1,29 @@
-function [outputArg1,outputArg2] = bessel_test(inputArg1,inputArg2)
+function [J] = bessel_test(nu)
 %BESSEL_TEST Testing routine for Bessel functions
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+%   
+
+    % Domain for Bessel function
+    z0 = 0.0;
+    z1 = 20;
+    npts = 201;
+    dz = (z1 - z0)/(npts - 1);
+    
+    z = z0:dz:z1;
+
+    J = zeros(1, npts);
+    
+    J(1,:) = besselj(nu,z);
+    
+    figure
+    plot(z,J)
+    grid on
+
+    title(['Bessel Function of the First Kind for $\nu = ' num2str(nu) '$'],'interpreter','latex')
+    xlabel('$z$','interpreter','latex')
+    ylabel('$J_\nu(z)$','interpreter','latex')
+    
+    
+
+
 end
 
