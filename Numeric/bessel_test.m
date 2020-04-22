57d23497f8835c571a504fdfd61b9f7d37c95285
M.P. Vaughan <martinpaul.vaughan@yahoo.com>
2020-04-22 17:34:17 +0100

    Deleted bessel_test

--- Numeric/bessel_test.m
+++
@@ -1,7 +0,0 @@
-function [outputArg1,outputArg2] = bessel_test(inputArg1,inputArg2)
-%BESSEL_TEST Testing routine for Bessel functions
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
-
