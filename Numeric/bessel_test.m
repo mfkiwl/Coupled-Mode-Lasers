b7c3d7b5db2ee4168cc288d5f9755fd3cc19193f
M.P. Vaughan <martinpaul.vaughan@yahoo.com>
2020-04-22 17:36:21 +0100

    Added bessel_test.m - a testing routine for Bessel functions

---
+++ Numeric/bessel_test.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = bessel_test(inputArg1,inputArg2)
+%BESSEL_TEST Testing routine for Bessel functions
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
